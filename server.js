bfbf4af68924eaf89c164fdec183f5c9b5488656
rohit.chandupatla@gmail.com <rohit.chandupatla@gmail.com>
2021-11-05 17:33:11 -0500

    changed app name in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/habit-remainder-client'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/habit-remainder-client/'}),
 );
 
 // Start the app by listening on the default Heroku port

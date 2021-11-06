4ea63ab42449a484b1c94c7cb90ac8c1c98b6adb
rohit.chandupatla@gmail.com <rohit.chandupatla@gmail.com>
2021-11-05 19:37:23 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
 import {SignUpComponent} from './sign-up/sign-up.component';
 const routes: Routes = [
+  {path:'/',component:HomeComponent},
   {path:'home',component:HomeComponent},
   {path:'login',component:LoginComponent},
   {path:'signup',component:SignUpComponent}

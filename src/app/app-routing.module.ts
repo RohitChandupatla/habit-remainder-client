07bf55d12f004d1d2e1b97d2653d44a5f55e4330
rohit.chandupatla@gmail.com <rohit.chandupatla@gmail.com>
2021-11-19 02:04:04 -0600

    added reset form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { HabitDetailsComponent } from './habit-details/habit-details.component';
 import { HabitListComponent } from './habit-list/habit-list.component';
 import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
+import { ResetComponent } from './reset/reset.component';
 import {SignUpComponent} from './sign-up/sign-up.component';
 const routes: Routes = [
   {path:'',component:HomeComponent},
@@ -16,6 +17,7 @@ const routes: Routes = [
   { path: 'add', component: AddHabitComponent },
   { path: 'update/:id', component: EditHabitComponent },
   { path: 'details/:id', component: HabitDetailsComponent },
+  { path: 'reset', component: ResetComponent },
 ];
 
 @NgModule({
